eaf26e5ddc4f1d6ce0a7ab6bba4b3034a97ff70a
[REVU Anh Duy] <anhduy@revu.com>
2023-08-01 08:58:42 +0700

    svelte i18 tailwindcss

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { t } from "$lib/translations";
+
+    const pageName = "This page is Home page!";
+</script>
+
+<div>
+    <!-- you can use `placeholders` and `modifiers` in your definitions (see docs) -->
+    <h2>{$t("common.title", { pageName })}</h2>
+    <p>{$t("common.title")}</p>
+</div>
