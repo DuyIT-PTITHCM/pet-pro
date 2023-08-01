8e964490cd77949dec9735f16716d9c6d2283863
[REVU Anh Duy] <anhduy@revu.com>
2023-08-01 10:13:36 +0700

    restruct

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
 <script>
     import { t } from "$lib/translations";
+    import OutsideClick from "../components/common/OutsideClick.svelte";
+    import { count } from "../store";
 
     const pageName = "This page is Home page!";
+
+    function increment() {
+        count.update((n) => n + 1); // Tăng giá trị của store lên 1
+    }
+    function handleOutsideClick() {
+        console.log("Clicked outside the component.");
+    }
 </script>
 
 <div>
     <!-- you can use `placeholders` and `modifiers` in your definitions (see docs) -->
-    <h2>{$t("common.title", { pageName })}</h2>
+    <h2>{ pageName }</h2>
     <p>{$t("common.title")}</p>
+    <button on:click={increment}>Increment</button>
+    <p>{$count}</p>
 </div>
