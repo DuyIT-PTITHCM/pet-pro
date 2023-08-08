3bbdbf5a300c43f56951763bb9ea186c790285e5
[REVU Anh Duy] <anhduy@revu.com>
2023-08-08 20:55:25 +0700

    setup server

---
+++ src/lib/components/demo/Demo.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Carousel from 'svelte-carousel';
+    import { browser } from '$app/environment';
+  
+    let carousel; // for calling methods of the carousel instance
+    
+    const handleNextClick = () => {
+      carousel.goToNext()
+    }
+  </script>
+  
+  {#if browser}
+    <Carousel
+      bind:this={carousel}
+    >
+      <div>
+        <h1>anh duy</h1>
+      </div>
+      <div>Nguyen</div>
+      <div>it</div>
+    </Carousel>
+  {/if}
+  
+  <button on:click={handleNextClick}>Next</button>
