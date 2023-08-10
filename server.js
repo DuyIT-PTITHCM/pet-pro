0ee523f431b556bfae603642596e191dbf062342
[REVU Anh Duy] <anhduy@revu.com>
2023-08-10 15:43:58 +0700

    running sveltekit with express only port

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-// server.js
 import express from 'express';
-
 const app = express();
-const port = process.env.PORT || 5100;
-// Your Express routes go here
-app.get('/api/data', (req, res) => {
-  // Handle your API logic here
-  res.json({ message: 'Hello from Express API!' });
+const PORT = process.env.PORT || 3000;
+import { handler } from './build/handler.js'; 
+
+// Định nghĩa các API endpoints hoặc các route khác tại đây
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use(handler);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
