25261c32697ab860147e6475ff18a151d04e17a3
[REVU Anh Duy] <anhduy@revu.com>
2023-08-15 11:30:53 +0700

    delete console

--- server/models/index.js
+++ server/models/index.js
@@ -3,10 +3,7 @@ import UserModel from './user.js';
 import dotenv from 'dotenv';
 dotenv.config(); 
 
-console.log(process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: 'mysql',
-  dialect: 'mysql',
-});
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: "mysql",
   dialect: 'mysql',
