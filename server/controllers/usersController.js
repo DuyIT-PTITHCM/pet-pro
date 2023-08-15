933e587358c1d51fa89c3eaccd432daa6ddcfd26
[REVU Anh Duy] <anhduy@revu.com>
2023-08-15 08:39:45 +0700

    implement nodemon

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -4,12 +4,5 @@ import { models, sequelize } from "../models/index.js";
 
 
 export const getAllUsers = async (req, res) => {
-    console.log( models.User.findAll());
     res.json({ message: await models.User.findAll() });
-    //   try {
-    //     const users = await User.findAll();
-    //     res.status(200).json(users);
-    //   } catch (error) {
-    //     res.status(500).json({ error: 'Internal server error' });
-    //   }
 };
