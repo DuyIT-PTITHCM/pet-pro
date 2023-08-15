eec2294f1b1002ac84c0ef3fb9a517dc1d16fe2f
[REVU Anh Duy] <anhduy@revu.com>
2023-08-15 22:17:20 +0700

    cc

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
-import { handler } from './build/handler.js';
+// import { handler } from './build/handler.js';
 import dotenv from 'dotenv';
 
 // Load biến môi trường từ tệp .env vào process.env
@@ -11,9 +11,12 @@ import usersRouter from './server/routes/api/v1.0/userRoute.js';
 
 // router ViewMode_1.0 
 app.use('/api/v1.0/users', usersRouter);
+app.use('/', (req, res) => {
+  res.json({ message: "say hello" });
+});
 
 
-app.use(handler);
+// app.use(handler);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
