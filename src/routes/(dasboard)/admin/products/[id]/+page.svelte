dd62363c8803c0fdd770870abc9923d3359a494c
[REVU Anh Duy] <anhduy@revu.com>
2023-09-01 01:01:12 +0700

    tmp

---
+++ src/routes/(dasboard)/admin/products/[id]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { RepositoryFactory } from "$lib/ClientService/RepositoryFactory";
+
+    const productService = RepositoryFactory.get('productRepository');
+    let product;
+    async function productDetail(){
+        product = await productService.show(12);
+        console.log(product);
+    }
+
+    productDetail();
+</script>
+<h1>alo</h1>
