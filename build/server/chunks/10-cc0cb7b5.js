59f1328540d26ee21dc6ea7e1c2c24dcb0732e5c
[REVU Anh Duy] <anhduy@revu.com>
2023-09-04 23:26:01 +0700

    build

---
+++ build/server/chunks/10-cc0cb7b5.js
@@ -0,0 +1,21 @@
+const load = ({ params }) => {
+  return {
+    id: params?.id
+  };
+};
+
+var _page_ts = /*#__PURE__*/Object.freeze({
+  __proto__: null,
+  load: load
+});
+
+const index = 10;
+let component_cache;
+const component = async () => component_cache ??= (await import('./_page.svelte-af696fec.js')).default;
+const universal_id = "src/routes/(dasboard)/admin/products/[id]/+page.ts";
+const imports = ["_app/immutable/nodes/10.e01b55d0.js","_app/immutable/chunks/scheduler.aa61743c.js","_app/immutable/chunks/index.8779bb2d.js","_app/immutable/chunks/accounting.14438540.js","_app/immutable/chunks/_commonjsHelpers.725317a4.js","_app/immutable/chunks/loading.9233548d.js","_app/immutable/chunks/index.138245f1.js","_app/immutable/chunks/RepositoryFactory.a9e58442.js","_app/immutable/chunks/cookieUtils.9487ed92.js","_app/immutable/chunks/axios.82afda87.js","_app/immutable/chunks/Indicator.svelte_svelte_type_style_lang.ea3d09bf.js","_app/immutable/chunks/CreateProduct.3d56f2e0.js","_app/immutable/chunks/globals.7f7f1b26.js","_app/immutable/chunks/Const.0f635b1d.js","_app/immutable/chunks/toastError.0a46b671.js","_app/immutable/chunks/Icon.0610134f.js","_app/immutable/chunks/Fileupload.5dfa137f.js","_app/immutable/chunks/Input.c3f894f8.js","_app/immutable/chunks/Wrapper.67c060db.js","_app/immutable/chunks/moment.fbc5633a.js"];
+const stylesheets = ["_app/immutable/assets/10.e3b0c442.css","_app/immutable/assets/Indicator.be78b448.css"];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets, _page_ts as universal, universal_id };
+//# sourceMappingURL=10-cc0cb7b5.js.map
---
+++ build/server/chunks/13-fb0f6bcb.js
@@ -0,0 +1,9 @@
+const index = 13;
+let component_cache;
+const component = async () => component_cache ??= (await import('./_page.svelte-7c2b0469.js')).default;
+const imports = ["_app/immutable/nodes/13.13611ebe.js","_app/immutable/chunks/scheduler.aa61743c.js","_app/immutable/chunks/index.8779bb2d.js","_app/immutable/chunks/Button.b4fe983a.js","_app/immutable/chunks/Indicator.svelte_svelte_type_style_lang.ea3d09bf.js","_app/immutable/chunks/DarkMode.45db212b.js","_app/immutable/chunks/Checkbox.5047436a.js","_app/immutable/chunks/Input.c3f894f8.js","_app/immutable/chunks/Wrapper.67c060db.js","_app/immutable/chunks/axiosServer.08f43fb5.js","_app/immutable/chunks/axios.82afda87.js","_app/immutable/chunks/Const.0f635b1d.js","_app/immutable/chunks/cookieUtils.9487ed92.js","_app/immutable/chunks/toastError.0a46b671.js","_app/immutable/chunks/index.138245f1.js","_app/immutable/chunks/loading.9233548d.js","_app/immutable/chunks/ToastError.8c250441.js","_app/immutable/chunks/Icon.0610134f.js"];
+const stylesheets = ["_app/immutable/assets/Indicator.be78b448.css","_app/immutable/assets/DarkMode.92db14e7.css","_app/immutable/assets/ToastError.be81d463.css"];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets };
+//# sourceMappingURL=13-fb0f6bcb.js.map
