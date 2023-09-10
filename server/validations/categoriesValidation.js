f73e63e991b361f700698e85c27c8b1b1d4940f4
[REVU Anh Duy] <anhduy@revu.com>
2023-09-10 22:22:12 +0700

    update api

--- server/validations/categoriesValidation.js
+++ server/validations/categoriesValidation.js
@@ -7,12 +7,10 @@ import { checkIdExits } from '../repositories/menuRepository.js';
 
 export const createCategoryValidation = [
     body('categoryName').notEmpty().isString().custom(isUniqueCategoryName),
-    body('type').notEmpty().isString().isIn(['product', 'article']),
     body('menuId').notEmpty().isInt().custom(checkIdExits)
 ];
 
 export const updateCategoryValidation = [
     body('categoryName').notEmpty().isString().custom(isUniqueCategoryNameUpdate),
-    body('type').notEmpty().isString().isIn(['product', 'article']),
     body('menuId').notEmpty().isInt().custom(checkIdExits)
 ];
