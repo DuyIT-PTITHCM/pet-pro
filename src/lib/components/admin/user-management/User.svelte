79e6f956819035beb57e544df2ed075a7e4debae
[REVU Anh Duy] <anhduy@revu.com>
2023-09-10 21:37:55 +0700

    update user management

--- src/lib/components/admin/user-management/User.svelte
+++ src/lib/components/admin/user-management/User.svelte
@@ -8,14 +8,18 @@
     import { isUserEdited } from "$lib/store/userManagement";
     import { onMount } from "svelte";
     import Icon from "@iconify/svelte";
-    import { getAllQueryParams, queryParamsToObject, updateQueryParams } from "$lib/Utils/queryParams";
+    import {
+        getAllQueryParams,
+        queryParamsToObject,
+        updateQueryParams,
+    } from "$lib/Utils/queryParams";
+    import Pagination from "$lib/components/common/Pagination.svelte";
 
     let isFilter = false;
     const userService = RepositoryFactory.get("userRepository");
 
-    let helper = { currentPage: 1, pages: 10, total: 100 };
     let queryParams = {
-        page: helper.currentPage, // Example query parameter
+        page: 1, // Example query parameter
         email: null,
         gender: null,
         phone: null,
@@ -24,14 +28,13 @@
 
     let users;
     loadingState.set(true);
-    async function getUsers() {   
-        loadingState.set(true);    
+    async function getUsers() {
+        loadingState.set(true);
         let queryFilter = getAllQueryParams();
         queryParams = queryParamsToObject(queryFilter);
         const res = await userService.get(queryParams);
-        users = res.data.data.docs;
-        helper.total = res.data.data.total;
-        helper.pages = res.data.data.pages;
+        users = res.data.data;
+
         loadingState.set(false);
     }
     onMount(() => {
@@ -44,29 +47,16 @@
     });
     getUsers();
 
-    async function filter (){
-        await  updateQueryParams(queryParams);
+    async function filter() {
+        await updateQueryParams(queryParams);
+        await getUsers();
+    }
+    async function handlePageChange(page) {
+        queryParams.page = page;
+        updateQueryParams(queryParams);
         await getUsers();
     }
 
-    // pagination
-    let isPrev = true;
-    let isNext = false;
-    const previous = () => {
-        helper.currentPage = Math.max(helper.currentPage - 1, 1);
-        queryParams.page = helper.currentPage;
-        isPrev = helper.currentPage == 1 ? true : false;
-        isNext = false;
-        getUsers();
-    };
-
-    const next = () => {
-        helper.currentPage = Math.min(helper.currentPage + 1, helper.pages);
-        queryParams.page = helper.currentPage;
-        isNext = helper.currentPage == helper.pages ? true : false;
-        isPrev = false;
-        getUsers();
-    };
 </script>
 
 <div class="header-manager bg-slate-100 dark:bg-slate-900 p-10 my-4 rounded-xl">
@@ -100,55 +90,14 @@
         <h1>nodata</h1>
     {:else if !$loadingState}
         <div class="overflow-hidden">
-            <UserList items={users} />
+            <UserList items={users.docs} />
         </div>
     {/if}
-    {#if helper.pages > 1}
-        <div class="flex flex-col items-center justify-center gap-2">
-            <div class="text-sm text-gray-700 dark:text-gray-400">
-                Showing <span
-                    class="font-semibold text-gray-900 dark:text-white"
-                    >{helper.currentPage}</span
-                >
-                of
-                <span class="font-semibold text-gray-900 dark:text-white"
-                    >{helper.pages}</span
-                >
-                pages and
-                <span class="font-semibold text-gray-900 dark:text-white"
-                    >{helper.total}</span
-                >
-                Entries
-            </div>
-            <div class="flex">
-                <ButtonGroup>
-                    <Button
-                        class="py-1 px-4"
-                        color={"blue"}
-                        on:click={previous}
-                        disabled={isPrev}
-                        ><Icon
-                            class="text-xl mr-1"
-                            icon="emojione:baby-chick"
-                        /><Icon class="text-3xl" icon="twemoji:dog" /></Button
-                    >
-                    <Button
-                        class="py-1 px-4"
-                        color={"primary"}
-                        on:click={next}
-                        disabled={isNext}
-                        ><Icon
-                            class="text-3xl"
-                            icon="twemoji:cat"
-                            hFlip={true}
-                        /><Icon
-                            class="text-xl ml-1"
-                            icon="noto:fish"
-                            hFlip={true}
-                        /></Button
-                    >
-                </ButtonGroup>
-            </div>
-        </div>
+    {#if users?.pages}
+        <Pagination
+            currentPage={users.currentPage}
+            totalPages={users.pages}
+            onPageChange={handlePageChange}
+        />
     {/if}
 </div>
