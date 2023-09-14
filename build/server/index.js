ca167cdeda8fa313a428e139805c3b307293482b
[REVU Anh Duy] <anhduy@revu.com>
2023-09-14 09:25:04 +0700

    build

--- build/server/index.js
+++ build/server/index.js
@@ -218,7 +218,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1r2ioz8"
+  version_hash: "1dlism2"
 };
 function get_hooks() {
   return import('./chunks/hooks.server-7c0cd223.js');
