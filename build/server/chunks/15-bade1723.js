91278c92573b7d1a23a88a6bd25f55f25a60630a
[REVU Anh Duy] <anhduy@revu.com>
2023-09-27 21:06:46 +0700

    build

---
+++ build/server/chunks/15-bade1723.js
@@ -0,0 +1,21 @@
+const load = ({ params }) => {
+  return {
+    id: params?.id
+  };
+};
+
+var _page_ts = /*#__PURE__*/Object.freeze({
+  __proto__: null,
+  load: load
+});
+
+const index = 15;
+let component_cache;
+const component = async () => component_cache ??= (await import('./_page.svelte-87c24f68.js')).default;
+const universal_id = "src/routes/(dasboard)/admin/blog/[id]/+page.ts";
+const imports = ["_app/immutable/nodes/15.028e758a.js","_app/immutable/chunks/scheduler.a17acf7c.js","_app/immutable/chunks/index.8874627e.js","_app/immutable/chunks/meta.392f5498.js","_app/immutable/chunks/index.0eb19cf3.js","_app/immutable/chunks/loading.1f962269.js","_app/immutable/chunks/RepositoryFactory.b431bac8.js","_app/immutable/chunks/Const.9014b5a8.js","_app/immutable/chunks/cookieUtils.9487ed92.js","_app/immutable/chunks/axios.82afda87.js","_app/immutable/chunks/Indicator.svelte_svelte_type_style_lang.caff1904.js","_app/immutable/chunks/DeviceMockup.97a1f34d.js","_app/immutable/chunks/Tabs.ae03dff6.js","_app/immutable/chunks/CreateArticle.6e0f0c61.js","_app/immutable/chunks/globals.7f7f1b26.js","_app/immutable/chunks/Editor.9d32c105.js","_app/immutable/chunks/toastError.0b7d6c81.js","_app/immutable/chunks/Fileupload.bf543ea6.js","_app/immutable/chunks/Input.7343cea9.js","_app/immutable/chunks/Wrapper.3159baba.js","_app/immutable/chunks/navigation.caf678a2.js","_app/immutable/chunks/singletons.0f801202.js","_app/immutable/chunks/CreateSeo.fc5d8ee6.js"];
+const stylesheets = ["_app/immutable/assets/Indicator.be78b448.css"];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets, _page_ts as universal, universal_id };
+//# sourceMappingURL=15-bade1723.js.map
--- build/server/chunks/16-d27ebefe.js
+++
@@ -1,9 +0,0 @@
-const index = 16;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_page.svelte-ddeebe86.js')).default;
-const imports = ["_app/immutable/nodes/16.f5170d37.js","_app/immutable/chunks/scheduler.a17acf7c.js","_app/immutable/chunks/index.8874627e.js","_app/immutable/chunks/CreateArticle.8f33dfff.js","_app/immutable/chunks/globals.7f7f1b26.js","_app/immutable/chunks/RepositoryFactory.b431bac8.js","_app/immutable/chunks/Const.9014b5a8.js","_app/immutable/chunks/cookieUtils.9487ed92.js","_app/immutable/chunks/axios.82afda87.js","_app/immutable/chunks/loading.1f962269.js","_app/immutable/chunks/index.0eb19cf3.js","_app/immutable/chunks/Editor.9d32c105.js","_app/immutable/chunks/toastError.0b7d6c81.js","_app/immutable/chunks/Indicator.svelte_svelte_type_style_lang.caff1904.js","_app/immutable/chunks/Fileupload.bf543ea6.js","_app/immutable/chunks/Input.7343cea9.js","_app/immutable/chunks/Wrapper.3159baba.js","_app/immutable/chunks/navigation.c8bfd593.js","_app/immutable/chunks/singletons.650480d4.js"];
-const stylesheets = ["_app/immutable/assets/Indicator.be78b448.css"];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=16-d27ebefe.js.map
