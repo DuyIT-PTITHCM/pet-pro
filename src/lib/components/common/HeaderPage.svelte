4eeec4cf057c5836b41168c450cacc55fd434372
Tri Huynh <tri.huynh@siliconstack.com.au>
2023-10-16 22:11:28 +0700

    duy lo

--- src/lib/components/common/HeaderPage.svelte
+++ src/lib/components/common/HeaderPage.svelte
@@ -6,14 +6,15 @@
     export let isBgOverlay = false;
     export let author = "Pet One";
     export let titleClass = false;
-    export let date = new Date().toDateString();
-    function formatDate(inputDate: string){
-        const newDate = new Date(inputDate);
-        const day = newDate.getDate().toString().padStart(2, '0');
-        const month = (newDate.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based, so we add 1
-        const year = newDate.getFullYear();
-        return `${day}/${month}/${year}`;
-    }
+    export let date = '';
+    // export let date = new Date().toDateString();
+    // function formatDate(inputDate: string){
+    //     const newDate = new Date(inputDate);
+    //     const day = newDate.getDate().toString().padStart(2, '0');
+    //     const month = (newDate.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based, so we add 1
+    //     const year = newDate.getFullYear();
+    //     return `${day}/${month}/${year}`;
+    // }
 </script>
 <div class="w-full h-[360px] bg-fixed bg-no-repeat bg-cover bg-center" style="background-image: url({image});">
     <div class="{isBgOverlay && 'bg-title'} w-full h-full">
@@ -28,7 +29,8 @@
                 {/if}
                 <div class="font-bold flex {isShowTime == false && 'hidden'}">
                     <p class="flex items-center border-x px-2"><Icon icon="fa6-solid:user-pen" class="mr-2 text-xl w-8"/> {author}</p>
-                    <p class="flex items-center border-x px-2"><Icon icon="svg-spinners:clock" class="mr-2 text-xl w-8"/>{date ? formatDate(date) : date}</p>
+                    <!-- <p class="flex items-center border-x px-2"><Icon icon="svg-spinners:clock" class="mr-2 text-xl w-8"/>{date ? formatDate(date) : date}</p> -->
+                    <p class="flex items-center border-x px-2"><Icon icon="svg-spinners:clock" class="mr-2 text-xl w-8"/>{date}</p>
                 </div>
             </div>
         </div>
