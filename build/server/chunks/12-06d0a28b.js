70886ece2893ac606b82489c61377b66d6ecd3c6
[REVU Anh Duy] <anhduy@revu.com>
2023-10-16 22:13:44 +0700

    build

---
+++ build/server/chunks/12-06d0a28b.js
@@ -0,0 +1,33 @@
+import { B as BASE_API } from './Const-aff6847f.js';
+
+const load = async ({ fetch, params }) => {
+  const slug = params.slug;
+  try {
+    const response = await fetch(`${BASE_API}/front/service/${slug}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await response.json();
+    return { ...data };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+var _page_server_ts = /*#__PURE__*/Object.freeze({
+  __proto__: null,
+  load: load
+});
+
+const index = 12;
+let component_cache;
+const component = async () => component_cache ??= (await import('./_page.svelte-c59361c4.js')).default;
+const server_id = "src/routes/(app)/dich-vu/[slug]/+page.server.ts";
+const imports = ["_app/immutable/nodes/12.75b579a8.js","_app/immutable/chunks/scheduler.12c735be.js","_app/immutable/chunks/index.c6264268.js","_app/immutable/chunks/globals.7f7f1b26.js","_app/immutable/chunks/HeaderPage.f8223aa2.js","_app/immutable/chunks/Indicator.svelte_svelte_type_style_lang.fa008dc3.js","_app/immutable/chunks/Services.7ff99ae5.js","_app/immutable/chunks/await_block.65442e8f.js","_app/immutable/chunks/Button.15a7e48e.js","_app/immutable/chunks/CardPlaceholder.90d3a433.js","_app/immutable/chunks/Const.4ee2e7e1.js"];
+const stylesheets = ["_app/immutable/assets/HeaderPage.42128702.css","_app/immutable/assets/Indicator.be78b448.css","_app/immutable/assets/Services.28274980.css"];
+const fonts = [];
+
+export { component, fonts, imports, index, _page_server_ts as server, server_id, stylesheets };
+//# sourceMappingURL=12-06d0a28b.js.map
---
+++ build/server/chunks/20-3ade3ecb.js
@@ -0,0 +1,9 @@
+const index = 20;
+let component_cache;
+const component = async () => component_cache ??= (await import('./_page.svelte-9654c515.js')).default;
+const imports = ["_app/immutable/nodes/20.6e6f7558.js","_app/immutable/chunks/scheduler.12c735be.js","_app/immutable/chunks/index.c6264268.js","_app/immutable/chunks/CreateArticle.d8ef0825.js","_app/immutable/chunks/globals.7f7f1b26.js","_app/immutable/chunks/RepositoryFactory.6988031d.js","_app/immutable/chunks/Const.4ee2e7e1.js","_app/immutable/chunks/cookieUtils.9487ed92.js","_app/immutable/chunks/axios.82afda87.js","_app/immutable/chunks/loading.7b8fb688.js","_app/immutable/chunks/index.9d0d763a.js","_app/immutable/chunks/Editor.4169feb9.js","_app/immutable/chunks/toastError.aad4e73c.js","_app/immutable/chunks/Indicator.svelte_svelte_type_style_lang.fa008dc3.js","_app/immutable/chunks/Fileupload.ae9f2eaa.js","_app/immutable/chunks/Input.ac251b9f.js","_app/immutable/chunks/Wrapper.787308e2.js","_app/immutable/chunks/navigation.c24db982.js","_app/immutable/chunks/singletons.6ba9044d.js","_app/immutable/chunks/index.039cfffe.js","_app/immutable/chunks/preload-helper.a4192956.js"];
+const stylesheets = ["_app/immutable/assets/Indicator.be78b448.css"];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets };
+//# sourceMappingURL=20-3ade3ecb.js.map
